5c066e6924f8c763e74044f3b4315b36f908a522
pengtikui <949828390@qq.com>
2016-01-09 21:18:37 +0800

    tencent 404 page

--- 404.php
+++ 404.php
@@ -1,17 +1,1 @@
-<?php get_header(); ?>
-
-<div class="main-content-wrap">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8">
-				<div class="archive-title"><?php _e('404 Not Found', 'paranoid') ?></div>
-				<div><p><?php _e('对不起，未找到此页面！', 'paranoid') ?></p></div>
-			</div>
-			<div class="col-md-4">
-				<?php get_sidebar(); ?>
-			</div>
-		</div>
-	</div>
-</div>
-
-<?php get_footer(); ?>
+<script type="text/javascript" src="http://www.qq.com/404/search_children.js" charset="utf-8" homePageUrl="<?php bloginfo('url'); ?>" homePageName="回到<?php bloginfo('name'); ?>首页"></script>
